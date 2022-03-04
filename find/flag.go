e2a1ca11f3d0be6bfdedff371ddcce2427ca3b46
Automne <l3085208223@163.com>
2022-03-04 16:13:37 +0800

    添加ip文件扫描

--- find/flag.go
+++ find/flag.go
@@ -6,6 +6,7 @@ import (
 
 func Flag(Info *HostInfo) {
 	flag.StringVar(&Info.Host, "h", "", "IP Address: 192.168.11.11 | 192.168.11.11-255 | 192.168.11.11,192.168.11.12")
+	flag.StringVar(&Info.HostFile, "hf", "", "IP File: ips.txt")
 	flag.StringVar(&Info.Ports, "p", "50000", "Ports Range: 50000 | 40000-65535 | 49440,51731,52841")
 	flag.StringVar(&Info.Type, "t", "", "Choose Attack Type: scan | rce")
 	flag.StringVar(&Info.Cmd, "c", "", "Input Cmd Command")
